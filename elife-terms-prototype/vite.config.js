c6f8fe7144f6259b07af78a7881ff52b2b0504c8
Chris huggins <c.huggins@elifesciences.org>
2026-08-20 11:52:42 +0100

    Fix Vite base path for GitHub Pages

--- elife-terms-prototype/vite.config.js
+++ elife-terms-prototype/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/elife-terms-prototype/',
+  base: '/ux-prototypes/elife-terms-prototype/',
 })
